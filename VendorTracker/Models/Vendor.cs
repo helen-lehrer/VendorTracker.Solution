dfd1a025b79610e42a06519421adb8cd6d334ce8
Helen Lehrer <hlehrer@outlook.com>
2022-09-30 09:43:49 -0700

    Pass 2nd test for VendorTests

--- VendorTracker/Models/Vendor.cs
+++ VendorTracker/Models/Vendor.cs
@@ -7,18 +7,18 @@ namespace VendorTracker.Models
     public string Name { get; set; }
     public string Description { get; set; }
     public string PhoneNumber { get; set; }
-    public string Id { get; }
+    public int Id { get; }
     public List<Order> Orders { get; set; }
     private static List<Vendor> _instances = new List<Vendor>{};
 
     public Vendor(string name, string description, string phoneNumber)
     {
-      // Name = name;
-      // Description = description;
-      // PhoneNumber = phoneNumber;
-      // _instances.Add(this);
-      // Id = _instances.Count;
-      // Orders = new List<Order>{};
+      Name = name;
+      Description = description;
+      PhoneNumber = phoneNumber;
+      _instances.Add(this);
+      Id = _instances.Count;
+      Orders = new List<Order>{};
     }
   }
 }
