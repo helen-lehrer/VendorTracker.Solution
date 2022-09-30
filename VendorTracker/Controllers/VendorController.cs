20a18f5c860e61fd60d78e2e5c9f424a3a0e954d
Helen Lehrer <hlehrer@outlook.com>
2022-09-30 13:07:33 -0700

    Add Home & Vendor Index

--- VendorTracker/Controllers/VendorController.cs
+++ VendorTracker/Controllers/VendorController.cs
@@ -9,11 +9,24 @@ namespace VendorTracker.Controllers
 {
     public class VendorController : Controller
     {
-        [HttpGet("/vendors")]
+        [HttpGet("/vendor")]
         public ActionResult Index()
         {
         List<Vendor> vendors = Vendor.GetAll(); 
         return View(vendors);
+        }
+
+        [HttpGet("/vendor/new")]
+        public ActionResult New()
+        {
+        return View();
+        }
+
+        [HttpPost("/vendor/")]
+        public ActionResult Create(string name, string description, string phoneNumber)
+        {
+        Vendor newVendor = new Vendor(name, description, phoneNumber);
+        return RedirectToAction("Index");
         }
     }
 }
